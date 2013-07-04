4eb8c45ce763b6a01673f978d3ff6f7778fe7f52
Huon Wilson <dbau.pp+github@gmail.com>
2013-07-04 22:07:56 +1000

    New data.

--- mem/dl.sh
+++ mem/dl.sh
@@ -5,7 +5,9 @@ BASE_URL='http://hnn.mrsd.org/~cmr/'
 HIST_FILE=history.txt
 MEM_FILE=mem.json
 TIME_FILE=time.txt
+CI_FILE=commit_info.txt
 
+mkdir -p $DL_DIR
 cd $DL_DIR
 
 curl -s ${BASE_URL}${HIST_FILE} -o ${HIST_FILE}
@@ -13,8 +15,14 @@ curl -s ${BASE_URL}${HIST_FILE} -o ${HIST_FILE}
 # Check for any hashes that haven't been downloaded (i.e. there is no
 # directory with the same name)
 for hash in $(grep -v -f <(ls) history.txt); do
-    echo $hash
-    mkdir $hash
-    curl -s ${BASE_URL}${hash}/${MEM_FILE} -o ${hash}/${MEM_FILE}
-    curl -s ${BASE_URL}${hash}/${TIME_FILE} -o ${hash}/${TIME_FILE}
+    (
+        echo $hash
+        mkdir -p $hash
+        cd $hash
+        for f in $MEM_FILE $TIME_FILE $CI_FILE; do
+            curl -s ${BASE_URL}${hash}/$f -o $f
+        done
+    ) &
 done
+
+wait

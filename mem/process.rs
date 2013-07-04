a6b76f997cb8c5eab00bf3f5a9a9c7c00b2f1f2f
Huon Wilson <dbau.pp+github@gmail.com>
2013-07-04 19:55:55 +1000

    Detailed plots, comments, everything: it's amazing!

--- mem/process.rs
+++ mem/process.rs
@@ -1,10 +1,14 @@
+//! Converts the raw mem.json (etc) to a more useable form.
+
 extern mod extra;
 use extra::{json};
 use extra::serialize::{Decodable, Encodable};
-use std::{os, io, run, str, comm};
+use std::{os, io, comm};
 use std::hashmap::HashSet;
 
 mod line_simplify;
+
+// these reflect the structure of mem.json exactly
 
 #[deriving(Decodable)]
 struct CPUAcct {
@@ -25,6 +29,7 @@ struct Data {
     memory_data: ~[(f64, f64)],
 }
 
+// these form the format we wish to output.
 #[deriving(Encodable)]
 struct Output {
     summary: Summary,
@@ -40,6 +45,7 @@ struct Summary {
     pull_request: Option<uint>
 }
 
+// The list of all hashes that we know about.
 fn processing_possibilities() -> HashSet<~str> {
     let mut set = HashSet::new();
     for os::list_dir(&Path("data")).consume_iter().advance |hash| {
@@ -50,51 +56,38 @@ fn processing_possibilities() -> HashSet<~str> {
     set
 }
 
+// Read the current summary json file, or "make" a new one if it
+// doesn't work.
 fn load_summary(p: &Path) -> ~[Summary] {
     match do io::file_reader(p).map |rdr| {
         json::from_reader(*rdr).unwrap()
     } {
-        Err(e) => ~[],
+        Err(_) => ~[],
         Ok(json) =>  Decodable::decode(&mut json::Decoder(json))
     }
 }
 
+/// A list of commits we've already seen
 fn already_processed(summary: &[Summary]) -> ~[~str] {
     summary.iter().transform(|x| x.hash.to_owned()).collect()
 }
 
-
+/// Reduce the number of points, while (hopefully) maintaining a
+/// decent representation of the data
 fn simplify_memory_data(v: &[(f64, f64)]) -> ~[(f64, f64)] {
     //line_simplify::rdp(v, 0.001)
     line_simplify::visvalingam(v, 100000.0)
 }
 
-fn get_hash_timestamp(hash: &str) -> uint {
-    let run::ProcessOutput{status,output,error} =
-        run::process_output("git", [~"show", ~"-s", ~"--format=%at", hash.to_owned()]);
-    if status != 0 { fail!("git failed %d: %s", status, str::from_bytes(error)); }
-    let out = str::from_bytes(output);
-    FromStr::from_str(out.trim()).unwrap()
-}
-
 fn main() {
-    let rust_dir = match os::args() {
-        [_, dir, .. _] => Path(dir),
-        _ => match os::homedir() {
-            Some(home) => home.push("rust"),
-            None => fail!("no rust dir")
-        }
-    };
-
     let summary_path = Path("out/summary.json");
     let mut summary = load_summary(&summary_path);
 
+    // work out what we're going to process
     let mut to_process = processing_possibilities();
     for already_processed(summary).consume_iter().advance |hash| {
         to_process.remove(&hash);
     }
-
-    let current_dir = os::getcwd();
 
     let (p, c) = comm::stream();
     let c = comm::SharedChan::new(c);
@@ -105,6 +98,7 @@ fn main() {
         let cc = c.clone();
         println(hash);
 
+        // paralellism!
         do spawn {
             let mem_path = Path("data").push_many([hash.as_slice(), "mem.json"]);
             let ci_path = Path("data").push_many([hash.as_slice(), "commit_info.txt"]);
@@ -119,6 +113,7 @@ fn main() {
             };
 
             let pull_request = if author == "bors bors@rust-lang.org" {
+                // a bors commit, so extract the pull request
                 let leading_num = summary.slice_from("auto merge of #".len());
                 let non_num = leading_num.find(|c: char| !c.is_digit()).get_or_zero();
                 FromStr::from_str(leading_num.slice_to(non_num))
@@ -126,7 +121,7 @@ fn main() {
                 None
             };
 
-
+            // load the mem.json file.
             let json = do io::file_reader(&mem_path).map |rdr| {
                 json::from_reader(*rdr).unwrap()
             }.unwrap();
@@ -134,8 +129,9 @@ fn main() {
             let d: Data = Decodable::decode(&mut json::Decoder(json));
             let simple_mem = simplify_memory_data(d.memory_data);
 
-            let fname = Path("out").push(hash + ".json");
             let usage = d.cpuacct.usage as f64 * 1e-9;
+
+            // create & write the output
             let summary = Summary {
                 hash: hash.to_owned(),
                 timestamp: time,
@@ -148,12 +144,14 @@ fn main() {
                 summary: summary.clone()
             };
 
+            let fname = Path("out").push(hash + ".json");
             let out_f = io::file_writer(&fname, [io::Create, io::Truncate]).unwrap();
             out.encode(&mut json::Encoder(out_f));
             cc.send(summary);
         }
     }
 
+    // collect the summaries
     for num.times {
         summary.push(p.recv());
     }
--- mem/script.js
+++ mem/script.js
@@ -1,109 +1,352 @@
-var width = 1000,
+var width = 900,
     height = 400,
-    margin = {top: 20, right: 80, bottom: 30, left: 80};
+    margin = {top: 10, right: 40, bottom: 20, left: 50};
 
-var svg = d3.select("#summary").append("svg")
-          .attr("width", width + margin.left + margin.right)
-          .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var detail_elem = document.getElementById('detail');
+var text_details_elem = document.getElementById('text-details');
 
-var x = d3.time.scale()
-    .range([0, width]);
+/// d3 helpers.
+function Plot(elem, width, height, margin) {
+  return d3.select(elem).append("svg")
+               .attr("width", width + margin.left + margin.right)
+               .attr("height", height + margin.top + margin.bottom)
+             .append("g")
+               .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+}
 
-var y_mem = d3.scale.linear()
-    .range([height, 0]);
-var y_cpu_time = d3.scale.linear()
-    .range([height, 0]);
+function Line(x_scale, x_access, y_scale, y_access) {
+  return d3.svg.line()
+     .x(function(d) { return x_scale(x_access(d)); })
+     .y(function(d) { return y_scale(y_access(d)); });
+}
+function Axis(scale, orient) {
+  return d3.svg.axis().scale(scale).orient(orient)
+}
 
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
+function DrawAxis(svg, axis, text, klass, opts) {
+  opts = opts || {};
+  var g = svg.append("g")
+    .attr("class", "axis " + klass)
+    .attr("transform", "translate(" + (opts['translate-x'] || 0) +
+          "," + (opts['translate-y'] || 0) + ")");
+  g.call(axis);
+  if (text) {
+    g.append("text")
+      .attr("transform", "rotate(" + (opts['rotate'] || '0') + ")")
+      .attr("dy", opts['dy'] || '0pt')
+      .style("text-anchor", opts['text-anchor'] || 'end')
+      .text(text);
+  }
+  return g;
+}
 
-var yAxis_mem = d3.svg.axis()
-    .scale(y_mem)
-    .orient("left");
-var yAxis_cpu_time = d3.svg.axis()
-    .scale(y_cpu_time)
-    .orient("right");
+/// A label for a commit + (optional) associated pr
+function Label(d, linkify) {
+  function wrap(link, text) {
+    return linkify ? '<a href="' + link + '">' + text + '</a>' : text;
+  }
+  var ret = wrap('https://github.com/mozilla/rust/commit/' + d.hash, d.hash.substr(0, 7));
+  if (d.pull_request) {
+    ret += ' (' +
+         wrap('https://github.com/mozilla/rust/pull/' + d.pull_request,'#' + d.pull_request) + ')';
+  }
+  return ret;
+}
 
-function time(d) { return d.timestamp * 1000; }
-function mem(d) { return d.max_memory / (1024 * 1024); }
-function cpu_time(d) { return d.cpu_time; }
+/// Construct the little box on the side
+function TextDetail(hash, data) {
+  function li(contents, klass) {
+    var l = document.createElement('li');
+    l.innerHTML = contents;
+    l.classList.add(klass);
+    return l;
+  }
 
-var line_mem = d3.svg.line()
-    .x(function(d) { return x(time(d)); })
-    .y(function(d) { return y_mem(mem(d)); });
-var line_cpu_time = d3.svg.line()
-    .x(function(d) { return x(time(d)); })
-    .y(function(d) { return y_cpu_time(cpu_time(d)); });
+  var text = document.createElement('li');
+  text.classList.add('text-detail');
+  text.id = 'text-' + hash;
 
-d3.json("out/summary.json", function(err, summary) {
-  var x_range = d3.extent(summary, time),
-      x_low = x_range[0], x_high = x_range[1],
-      dx = x_high - x_low;
+  var h2 = document.createElement('h2');
+  h2.innerHTML = Label(data.summary, true);
+  text.appendChild(h2);
 
-  x.domain([x_low - 0.03*dx, x_high + 0.03*dx]);
-  var y_mem_max = d3.max(summary, mem);
-  y_mem.domain([0, y_mem_max]);
-  y_cpu_time.domain([0, d3.max(summary, cpu_time)]);
+  var clear = document.createElement('div');
+  clear.classList.add('clear-button');
+  clear.innerHTML = '✘';
+  clear.title = 'Remove this series';
+  clear.addEventListener('click', function() { detail_toggle(hash, true) });
+  text.appendChild(clear);
 
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+  var ul = document.createElement('ul');
+  text.appendChild(ul);
 
-  svg.append("g")
-      .attr("class", "y axis mem")
-      .call(yAxis_mem)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Memory (MiB)");
-  svg.append("g")
-      .attr("class", "y axis cpu")
-      .attr("transform", "translate(" + width + ",0)")
-      .call(yAxis_cpu_time)
-     .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 1)
-      .attr("dy", "-.9em")
-      .style("text-anchor", "end")
-      .text("CPU Time (s)");
+  if (data.summary.pull_request !== null) {
+    var pr = data.summary.pull_request;
+    var contents = pr_title_cache.has(pr) ? pr_title_cache.get(pr) : '';
+    var l = li(contents, 'pr-title');
+    if (contents == '') {
+      l.id = 'pr-title-' + pr;
+      l.classList.add('hidden');
+      var script = document.createElement('script');
+      script.src = 'https://api.github.com/repos/mozilla/rust/pulls/' + pr + '?callback=pr_callback';
+      document.body.appendChild(script);
+    }
+    ul.appendChild(l);
+  }
 
-  // vertical lines go under the graphs
-  var lines = svg.append('g');
+  ul.appendChild(
+    li('Max memory usage: ' + (data.summary.max_memory/(1024*1024)).toFixed(0) + ' MiB', 'mem-text'))
+  ul.appendChild(li('CPU time: ' + data.summary.cpu_time.toFixed(1) + ' s', 'cpu-text'));
 
-  svg.append("path")
-      .datum(summary)
-      .attr("class", "line mem")
-      .attr("d", line_mem);
-  svg.append("path")
-      .datum(summary)
-      .attr("class", "line cpu")
-      .attr("d", line_cpu_time);
+  text_details_elem.appendChild(text);
+}
 
-  summary.forEach(function(d) {
-    var xx = x(time(d));
-    var g = lines.append('g')
-       .attr("transform", "translate(" + xx +", 0)")
-       .attr('class', 'marker')
-       .on('click', function (a,b,c,d) {
-           console.log(a,b,c,d)
+// use the github api to get the titles of the PRs, but cache it, so
+// we're not hitting the api and the network constantly
+var pr_title_cache = d3.map();
+function pr_callback(json) {
+  var data = json.data;
+  if (data) {
+    var id = 'pr-title-' + data.number;
+    var span = document.getElementById(id);
+    span.innerHTML = data.title;
+    pr_title_cache.set(data.number, data.title);
+    span.classList.remove('hidden');
+  }
+}
+
+/// Convert a hash to a colour. This gives less nice results than
+/// using one of the d3 ones, but they are stable.
+function hash_to_colour(hash) {
+  var r = parseInt(hash.substr(0, 2), 16),
+      g = parseInt(hash.substr(2, 2), 16),
+      b = parseInt(hash.substr(4, 2), 16);
+
+  return 'rgb('+[r,g,b].join(',')+')';
+}
+
+/// Toggle whether a certain hash is displayed on the detailed plot.
+var detail_toggle = (function() {
+    var detail = Plot("#detail", width, height, margin);
+    var x = d3.scale.linear().range([0, width]);
+    var x_axis = Axis(x, "bottom");
+    var y = d3.scale.linear().range([height, 0]);
+    var y_axis = Axis(y, "left");
+
+    DrawAxis(detail, x_axis, '', 'x detail', {'translate-y': height});
+    DrawAxis(detail, y_axis, 'Memory (MiB)', 'y detail', {'dy': '1.5em', 'rotate': '-90'});
+
+    var detail_lines = detail.append("g");
+    var line = Line(x, d_time, y, d_mem);
+
+    function d_time(d) { return d[0]; }
+    function d_mem(d) { return d[1] / (1024 * 1024); }
+
+    // cache for the detailed information, again to avoid hitting the
+    // network.
+    var detail_cache = d3.map();
+
+    // the commits that currently are shown in detail. Actually maps
+    // hash to (max memory usage, elapsed time), to make bounds
+    // calculations faster.
+    var visible_details = d3.map();
+
+    /// register the clear all button handler here, since the
+    /// visible_details object is only in scope here.
+    var clear_all = document.getElementById('clear-all');
+    clear_all.addEventListener('click', function() {
+      visible_details.keys().forEach(function(hash) {
+        detail_toggle(hash, true);
+      });
+    })
+
+    return function(hash, adjust_hash) {
+      if (detail_cache.has(hash)) {
+        inner(detail_cache.get(hash));
+      } else {
+        d3.json("out/" + hash + ".json", function(e,d) {
+          if (e) {
+            console.warn("Error getting details", e);
+            return;
+          }
+
+          detail_cache.set(hash, d);
+          inner(d)
         });
-    g.append('line')
-       .attr('class', 'marker-line')
-       .attr('x1', 0).attr('x2', 0)
-       .attr('y1', height).attr('y2', 0);
+      }
 
-    g.append('text')
-      .attr("class", "hash")
-      .attr("transform", "rotate(-90)")
-      .attr('x', -height + 100)
-      .attr("dy", "0.3em")
-      .style("text-anchor", "end")
-      .text(d.hash.substr(0, 6));
-  })
-})
+      function inner(data) {
+        if (visible_details.has(hash)) {
+          // already visible, so remove it
+          setColour(hash, '');
+          visible_details.remove(hash);
+          var text = document.getElementById('text-' + hash);
+          text.parentNode.removeChild(text);
+        } else {
+          // not visible, so register it and it's description.
+          var x_max = d3.max(data.memory_data, d_time),
+              y_max = d3.max(data.memory_data, d_mem);
+          visible_details.set(hash, {y: y_max, x: x_max});
+          TextDetail(hash, data)
+        }
+
+        // update the hash & plots for the above change.
+
+        if (adjust_hash) {
+          var new_hash = visible_details.keys().map(function(s) {return s.substr(0, 7)}).join(',');
+          window.location.replace('#' + new_hash);
+        }
+
+        var x_max = d3.max(visible_details.values(), function(d) {return d.x}),
+            y_max = d3.max(visible_details.values(), function(d) {return d.y});
+        x.domain([0, x_max]).nice();
+        y.domain([0, y_max]).nice();
+
+        // colour the summary markers and the text detail border
+        visible_details.forEach(function (hash) {
+          setColour(hash, hash_to_colour(hash));
+        })
+
+        detail.select(".x.axis").call(x_axis);
+        detail.select(".y.axis").call(y_axis);
+        var selection = detail_lines.selectAll('.line.detail').data(visible_details.keys());
+
+        // remove old lines
+        selection.exit().remove();
+        // update existing (for scale changes, etc)
+        selection.call(draw_line);
+        // add new
+        selection.enter().append('path').call(draw_line);
+
+        // hide the graph if it's empty
+        if (visible_details.keys().length == 0) {
+          detail_elem.classList.add('hidden');
+        } else {
+          detail_elem.classList.remove('hidden');
+        }
+
+        function draw_line(selection) {
+          selection
+            .style('stroke', function(hash) { return hash_to_colour(hash); })
+            .datum(function(hash) { return detail_cache.get(hash).memory_data; })
+            .attr('class', 'line detail')
+            .attr('d', line);
+        }
+
+        function setColour(hash, colour) {
+          d3.select('#marker-' + hash).style('fill', colour);
+          d3.select('#marker-' + hash + ' .marker-line').style('stroke', colour);
+          document.getElementById('text-' + hash).style.borderColor = colour;
+        }
+      }
+    }
+})();
+
+// drawing the main summary plot
+(function() {
+    var summary = Plot("#summary", width, height, margin);
+
+    var x = d3.time.scale()
+        .range([0, width]);
+
+    var y_mem = d3.scale.linear().range([height, 0]);
+    var y_cpu_time = d3.scale.linear().range([height, 0]);
+
+    var xAxis = Axis(x, "bottom");
+
+    var yAxis_mem = Axis(y_mem, "left");
+    var yAxis_cpu_time = Axis(y_cpu_time, "right");
+
+    function time(d) { return d.timestamp * 1000; }
+    function mem(d) { return d.max_memory / (1024 * 1024); }
+    function cpu_time(d) { return d.cpu_time; }
+
+    var line_mem = Line(x, time, y_mem, mem);
+    var line_cpu_time = Line(x, time, y_cpu_time, cpu_time);
+
+    d3.json("out/summary.json", function(err, data) {
+      var x_low = d3.min(data, time),
+          x_high = Date.now(),
+          dx = x_high - x_low;
+
+      x.domain([x_low - 0.03*dx, x_high]);
+      y_mem.domain([0, d3.max(data, mem)]);
+      y_mem.nice();
+      y_cpu_time.domain([0, d3.max(data, cpu_time)]);
+      y_cpu_time.nice();
+
+      DrawAxis(summary, xAxis, '', 'x', {'translate-y': height});
+      DrawAxis(summary, yAxis_mem, 'Memory (MiB)', 'y mem', {
+        'rotate': '-90',
+        'dy': '1.5em'
+      });
+      DrawAxis(summary, yAxis_cpu_time, 'CPU Time (s)', 'y cpu', {
+        'rotate': '-90',
+        'translate-x': width,
+        'dy': '-.9em'
+      });
+
+      // vertical lines go under the graphs
+      var lines = summary.append('g');
+
+      summary.append("path")
+          .datum(data)
+          .attr("class", "line cpu")
+          .attr("d", line_cpu_time);
+      summary.append("path")
+          .datum(data)
+          .attr("class", "line mem")
+          .attr("d", line_mem);
+
+      // a map from the first 7 letters of each commit hash to the
+      // whole thing, used for the URL #
+      var short2long = d3.map();
+
+      // draw the vertical lines for each commit
+      data.forEach(function(d) {
+        var xx = x(time(d));
+        var g = lines.append('g')
+           .attr("transform", "translate(" + xx +", 0)")
+           .attr('id', 'marker-' + d.hash)
+           .attr('class', 'marker')
+           .on('click', function () {
+                  detail_toggle(d.hash, true)
+                });
+
+        // the line portion
+        g.append('line')
+           .attr('class', 'marker-line')
+           .attr('x1', 0).attr('x2', 0)
+           .attr('y1', height).attr('y2', 0);
+
+        // the text portion
+        var label = Label(d, false);
+
+        g.append('text')
+          .attr("class", "hash")
+          .attr("transform", "rotate(-90)")
+          .attr('x', -height)
+          .attr("dy", "0.3em")
+          .attr("dx", "1em")
+          .style("text-anchor", "start")
+          .text(label);
+
+        short2long.set(d.hash.substr(0, 7), d.hash);
+      })
+
+      // if the hash looks like #somehash,somehash,somehash, try to
+      // prefill the detailed plot
+      if (/^#[a-z0-9,]+$/.test(window.location.hash)) {
+        window.location.hash.substr(1).split(',').forEach(function (hash_prefix) {
+          if (hash_prefix.length >= 7) { // ignore things that are too short
+            detail_toggle(short2long.get(hash_prefix.substr(0, 7)));
+          } else {
+            console.warn("Hash prefix must to be >= 7 chars: '" + hash_prefix + "'");
+          }
+        })
+      } else {
+        // draw the last one
+        detail_toggle(data[data.length - 1].hash, false)
+      }
+    })
+})();
